74022b9143ceb8a20b3e06b3abf19395c7640bee
Steven Bishop <stevenbishopdeveloper@gmail.com>
2023-03-06 06:50:35 -0800

    Create reusbale page container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import PageContainer from "@/components/ui/page-container.tsx/page-container";
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return <PageContainer title="Suggestions"></PageContainer>;
 };
 
 export default HomePage;
