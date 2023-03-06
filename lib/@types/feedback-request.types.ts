8f996913bf190504bee2ff95503f70fd43ef3f1d
Steven Bishop <stevenbishopdeveloper@gmail.com>
2023-03-05 19:19:49 -0800

    Create types for feedback requests

---
+++ lib/@types/feedback-request.types.ts
@@ -0,0 +1,40 @@
+import { ObjectId } from "mongodb";
+
+type Category = "suggestion" | "ui" | "ux" | "enhancement" | "bug" | "feature";
+type Status = "planned" | "in-progress" | "live";
+
+export type CurrentUser = {
+  image: string;
+  name: string;
+  username: string;
+};
+
+export type User = {
+  image: string;
+  name: string;
+  username: string;
+};
+
+export type Reply = {
+  content: string;
+  replyingTo: string;
+  user: User;
+};
+
+export type Comment = {
+  id: number;
+  content: string;
+  user: User;
+  replies?: Reply[];
+};
+
+export type FeedbackRequest = {
+  _id?: ObjectId;
+  id: number;
+  title: string;
+  category: Category;
+  upvotes: number;
+  status: Status;
+  description: string;
+  comments: Comment[];
+};
